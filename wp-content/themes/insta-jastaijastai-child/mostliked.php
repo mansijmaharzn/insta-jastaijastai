ccdc4d0d77dcc0b0d287663094846f0e14404a97
Mansij Maharjan <mansijmaharzn@gmail.com>
2022-12-27 20:44:48 +0545

    add most viewed and liked

--- wp-content/themes/insta-jastaijastai-child/mostliked.php
+++ wp-content/themes/insta-jastaijastai-child/mostliked.php
@@ -4,6 +4,27 @@ get_header();
 require "config.php";
 ?>
 
+<?php
+    $select = $conn->query("SELECT * FROM posts ORDER BY likes");
+    $select->execute();
+    $rows = $select->fetchAll(PDO::FETCH_OBJ);
+    $rows = array_reverse($rows);
+?>
+
+<?php foreach($rows as $row) : ?>
+<div class="card">
+    <div class="card-header">
+    <?php echo $row->username . " " . $row->created_at; ?>
+    </div>
+    <div class="card-body">
+        <h5 class="card-title"><?php echo $row->title; ?></h5>
+        <p class="card-text"><?php echo substr($row->body, 0, 90) . '...'; ?></p>
+        <p>Likes: <?php echo $row->likes; ?></p>
+        <p>Views: <?php echo $row->views; ?></p>
+        <a href="/insta-jastaijastai/index.php/view-post?id=<?php echo $row->id; ?>" class="btn btn-primary">View Post</a>
+    </div>
+</div>
+<?php endforeach; ?>
 
 <!-- Footer -->
 <?php
