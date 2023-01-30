b3e24689c0e63f13eafc3e2906e65131474aabed
Mansij Maharjan <mansijmaharzn@gmail.com>
2023-01-30 15:25:19 +0545

    remove socials from footer

--- wp-content/themes/insta-jastaijastai/footer.php
+++ wp-content/themes/insta-jastaijastai/footer.php
@@ -12,40 +12,7 @@
 ?>
 	</div>
 	<!-- Footer -->
-<footer class="text-center text-lg-start bg-dark text-white mt-5">
-    <!-- Section: Social media -->
-    <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
-        <!-- Left -->
-        <div class="me-5 d-none d-lg-block">
-            <span>Get connected with us on social networks:</span>
-        </div>
-        <!-- Left -->
-
-        <!-- Right -->
-        <div>
-            <a href="#" class="me-4 text-reset">
-                <i class="fab fa-facebook-f"></i>
-            </a>
-            <a href="#" class="me-4 text-reset">
-                <i class="fab fa-twitter"></i>
-            </a>
-            <a href="#" class="me-4 text-reset">
-                <i class="fab fa-google"></i>
-            </a>
-            <a href="#" class="me-4 text-reset">
-                <i class="fab fa-instagram"></i>
-            </a>
-            <a href="#" class="me-4 text-reset">
-                <i class="fab fa-linkedin"></i>
-            </a>
-            <a href="https://github.com/mansijmaharzn/isp-ticketingSystem" target="_blank" class="me-4 text-reset">
-                <i class="fab fa-github"></i>
-            </a>
-        </div>
-        <!-- Right -->
-    </section>
-    <!-- Section: Social media -->
-
+<footer class="text-center text-lg-start bg-dark text-white mt-5 pt-3">
     <!-- Section: Links  -->
     <section class="footer-links">
         <div class="container text-center text-md-start mt-5">
