7111b554c4fc4436ceb3223c6763b6fa90669aad
Mansij Maharjan <mansijmaharzn@gmail.com>
2023-01-30 14:44:14 +0545

    enchance posts card

--- wp-content/themes/insta-jastaijastai-child/index.php
+++ wp-content/themes/insta-jastaijastai-child/index.php
@@ -11,17 +11,15 @@ require "config.php";
 ?>
 
 <?php foreach($rows as $row) : ?>
-<div class="card">
-    <div class="card-header">
-    <?php echo $row->username . " " . $row->created_at; ?>
-    </div>
-    <div class="card-body">
-        <h5 class="card-title"><?php echo $row->title; ?></h5>
-        <p class="card-text"><?php echo substr($row->body, 0, 90) . '...'; ?></p>
-        <p>Likes: <?php echo $row->likes; ?></p>
-        <p>Views: <?php echo $row->views; ?></p>
-        <a href="/insta-jastaijastai/index.php/view-post?id=<?php echo $row->id; ?>" class="btn btn-primary">View Post</a>
-    </div>
+
+<div class="rounded-5 my-4 p-5" style="background-color: #FFCDC4">
+<h6 class="float-end">By <?php echo $row->username . " on " . date('Y-m-d', strtotime($row->created_at)); ?></h6>
+<h1><?php echo $row->title; ?></h1>
+<p><?php echo substr($row->body, 0, 500) . '...'; ?></p>
+<h6>Likes: <?php echo $row->likes; ?></h6>
+<h6>Views: <?php echo $row->views; ?></h6>
+
+<a href="/insta-jastaijastai/index.php/view-post?id=<?php echo $row->id; ?>" class="btn btn-primary rounded-pill mt-2">View Post</a>
 </div>
 <?php endforeach; ?>
 
